6b8bd50e2aeaf3af47a21591cbf6afc31d2aa018
Diego Herrera <diegogaston4@gmail.com>
2021-10-21 12:00:42 -0300

    heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', function (req, res) {
     return JSON.stringify(req.body)
@@ -101,7 +103,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
